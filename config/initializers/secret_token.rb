7e1859331361a979da49e6b0fd79828e9f3407b4
Alejandra Gutierrez <miss_amisa@hotmail.com>
2013-10-24 15:36:03 -0500

    Create project rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RBartolo::Application.config.secret_key_base = 'a0e8e9ec370f68ff0bbee5d65f0f1f9c5f2baf93b5c54c8c3df1fde2c7dd6107d71986fe4288a0e08280927f0f84a30f79281b64b638980d65d7ad98801220f7'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RBartolo::Application.config.session_store :cookie_store, key: '_r-bartolo_session'
